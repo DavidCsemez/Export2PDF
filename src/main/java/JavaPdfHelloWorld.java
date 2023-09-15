e8ec2ee238faa1bdba322f14b49e0c5585d2e476
DavidCsemez <csemezdavid11@gmail.com>
2023-09-15 15:33:59 +0200

    Upravená verzia fontu na ARIAL

--- src/main/java/JavaPdfHelloWorld.java
+++ src/main/java/JavaPdfHelloWorld.java
@@ -1,24 +1,37 @@
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
 import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.BaseFont;
 import com.itextpdf.text.pdf.PdfWriter;
 
 public class JavaPdfHelloWorld {
     public static void main(String[] args) {
-            Document document = new Document();
-            try {
-                PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("HelloWorld.pdf"));
-                document.open();
-                document.add(new Paragraph("A Hello World PDF document."));
-                document.close();
-                writer.close();
-            } catch (DocumentException e) {
-                e.printStackTrace();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
+        Document document = new Document();
+
+        // -START- Inicializacia spravneho fontu
+        BaseFont baseFont = null;
+
+        try {
+            baseFont = BaseFont.createFont("ARIAL.TTF",BaseFont.IDENTITY_H,BaseFont.EMBEDDED);
+            Font font=new Font(baseFont, 12);
+            PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("HelloWorld.pdf"));
+            document.open();
+            document.add(new Paragraph("A Hello World PDF document loveäú+ľšľ+čťžťčýáíéýô).",font));
+            document.add(new Paragraph("Auto je cervene, nebo modre",font));
+            document.close();
+            writer.close();
+        } catch (DocumentException e) {
+            System.out.println("Problem pri generovani dokumentu");
+        } catch (FileNotFoundException e) {
+            System.out.println("Problem pri vytvarani suboru");
+        } catch (IOException e) {
+            System.out.println("Neznama chyba na vstupe/vystupe");
+        }
     }
 }
